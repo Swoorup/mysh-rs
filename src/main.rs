10bf1a277c5ee6dbb829f270a6eb545d9a7d3fe8
Swoorup Joshi <swoorupj@gmail.com>
2018-05-02 03:50:50 +0800

    prompt for saving prompt data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+#![allow(dead_code)]
+use std::io;
+use std::io::Write;
+
 mod prompt;
 
-fn main() {
+use prompt::Prompt;
 
-    println!("Hello, world!");
+fn ex_command(cmd: String) {
+    print!("You wrote {}", cmd)
+}
+
+fn main() {
+    let prompt = Prompt::new(String::from("swoorup % "));
+
+    loop {
+        print!("{}", prompt.get_prompt());
+        io::stdout().flush().expect("Failed to flush");
+
+        // read input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        ex_command(input);
+    }
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,14 +1,49 @@
-struct Prompt {
+pub struct Prompt {
     prompt_str: String,
-    signal_set: bool
 }
 
 impl Prompt {
-    fn new() -> Prompt {
-        Prompt{ prompt_str: String::from("%"), signal_set: false }
+    pub fn new(prompt_str: String) -> Prompt {
+        Prompt {
+            prompt_str: prompt_str,
+        }
     }
 
-    fn set_prompt(&mut self, new_prompt_str: String) {
-        *self.prompt_str = new_prompt_str;
+    pub fn set_prompt(&mut self, new_prompt_str: String) {
+        self.prompt_str = new_prompt_str;
     }
+
+    pub fn get_prompt(&self) -> &String {
+        &self.prompt_str
+    }
+
+    // just for fun, this is unnecessary
+    pub fn move_prompt(&mut self) -> String {
+        ::std::mem::replace(&mut self.prompt_str, String::new())
+    }
+}
+
+#[test]
+fn test_set_prompt() {
+    let prompt = Prompt::new(String::from("sytherax% "));
+    assert!(prompt.get_prompt().as_str() == "sytherax% ");
+
+    prompt.set_prompt(String::from("swoorup% "));
+    assert!(prompt.get_prompt().as_str() == "swoorup% ");
+}
+
+#[test]
+fn test_get_prompt() {
+    let prompt = Prompt::new(String::from("sytherax% "));
+    assert!(prompt.get_prompt().as_str() == "sytherax% ");
+    assert!(prompt.get_prompt().as_str() == "sytherax% ");
+}
+
+#[test]
+fn test_move_prompt() {
+    let mut prompt = Prompt::new(String::from("sytherax% "));
+
+    assert!(prompt.move_prompt().as_str() == "sytherax% ");
+    assert!(prompt.move_prompt().as_str() != "sytherax% ");
+    assert!(prompt.move_prompt().as_str() == "");
 }
