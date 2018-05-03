b2583176b9602c91eed252f96226262544e16f7c
Swoorup Joshi <swoorupj@gmail.com>
2018-05-04 04:53:30 +0800

    More concisenes :)

--- src/lexer.rs
+++ src/lexer.rs
@@ -41,42 +41,35 @@ impl <'a> LexicalAnalyzer <'a> {
     pub fn tokenize(&mut self, string: &'a str) {
         let mut it = string.chars().enumerate().peekable();
 
-        loop {
-            match it.peek() {
-                Some(&(i, ch)) => match ch {
-                    '\t' | ' ' => {
-                        // ignore if last token was whitespace
-                        match self.token_list.back() {
-                            Some(&Token::WhiteSpace) => (),
-                            _ => self.token_list.push_back(Token::WhiteSpace),
-                        }
+        while let Some((i, ch)) = it.next() {
+            match ch {
+                '\t' | ' ' => {
+                    // ignore if last token was whitespace
+                    match self.token_list.back() {
+                        Some(&Token::WhiteSpace) => (),
+                        _ => self.token_list.push_back(Token::WhiteSpace),
                     }
-                    '"' | '\'' => {
-                        // extract string literal in between quotes
-                        it.next();
-                        let c = it.position(|(_, _ch)| _ch == ch).unwrap();
-                        let (start, end) = (i+1, i+c+1);
-                        self.token_list.push_back(Token::VarString(&string[start..end]));
-                    }
-                    _ => {
-                        let remaining_str = &string[i..];
-                        match starts_with_symbol(remaining_str) {
-                            Some(s) => {
-                                let symbol_length = s.len();
-                                self.token_list.push_back(Token::Symbols(String::from(s)));
-                                for _ in 0..symbol_length {
-                                    it.next();
-                                }
-                            }
-                            None => println!("{}", &remaining_str),
-                        }
-                    },
                 }
-                None => break,
-            }
-            it.next();
+                '"' | '\'' => {
+                    // extract string literal in between quotes
+                    let c = it.position(|(_, _ch)| _ch == ch).unwrap();
+                    let (start, end) = (i+1, i+c+1);
+                    self.token_list.push_back(Token::VarString(&string[start..end]));
+                }
+                _ => {
+                    let remaining_str = &string[i..];
+                    if let Some(s) = starts_with_symbol(remaining_str) {
+                        let symbol_length = s.len();
+                        self.token_list.push_back(Token::Symbols(String::from(s)));
+                        for _ in 0..symbol_length {
+                            it.next();
+                        }
+                    } else {
+                        println!("{}", &remaining_str);
+                    }
+                },
+            } 
         }
-
     }
 }
 
