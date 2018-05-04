fa5cfc4e3bed65e761adeb2fb92ba5ff7e22a18a
Swoorup Joshi <swoorupj@gmail.com>
2018-05-04 15:06:17 +0800

    Now captures other tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,7 +4,7 @@ use std::vec;
 lazy_static! {
     static ref SYMBOLS: vec::Vec<&'static str> = {
         let mut m = vec!["&&", ";", "&", "|", ">", ">>", "<", "<<", "||"];
-    
+
         // sort for longest match rule
         m.sort_by(|a, b| b.cmp(a));
         m
@@ -12,61 +12,91 @@ lazy_static! {
 }
 
 fn starts_with_symbol(line: &str) -> Option<&'static str> {
-    SYMBOLS.iter().find(|&&sym| line.starts_with(sym)).map(|sym| *sym)
+    SYMBOLS
+        .iter()
+        .find(|&&sym| line.starts_with(sym))
+        .map(|sym| *sym)
 }
 
 #[derive(PartialEq, Debug)]
-enum Token <'a> {
+enum Token<'a> {
     WhiteSpace,
     Symbols(&'static str), // i.e ';', '&', etc
-    VarString(&'a str), // string slice representing commands, parameters to commands, etc
+    VarString(&'a str),    // string slice representing commands, parameters to commands, etc
 }
 
-pub struct LexicalAnalyzer <'a> {
+pub struct LexicalAnalyzer<'a> {
     token_list: LinkedList<Token<'a>>,
 }
 
-impl <'a> LexicalAnalyzer <'a> {
+impl<'a> LexicalAnalyzer<'a> {
     pub fn new() -> LexicalAnalyzer<'a> {
         LexicalAnalyzer {
-            token_list: LinkedList::new(), 
+            token_list: LinkedList::new(),
         }
     }
 
     pub fn tokenize(&mut self, string: &'a str) {
         let mut it = string.chars().enumerate().peekable();
-        let mut is_in_string = false;
-        // let terminate_and_push = || {
-        //     is_in_string = false;
 
-        // }
+        let mut start = 0;
+        let mut capturing = false;
 
-        while let Some((i, ch)) = it.next() {
-            match ch {
-                '\t' | ' ' => {
-                    // ignore if last token was whitespace
-                    if self.token_list.back() != Some(&Token::WhiteSpace) { 
+        loop {
+            let mut omit_current_ch_for_capture = true;
+
+            if let Some((i, ch)) = it.next() {
+                match ch {
+                    '\t' | ' ' => {
+                        // ignore if last token was whitespace
+                        // if self.token_list.back() != Some(&Token::WhiteSpace) {
                         self.token_list.push_back(Token::WhiteSpace);
+                        // }
                     }
-                }
-                '"' | '\'' => {
-                    // extract string literal in between quotes
-                    let c = it.position(|(_, _ch)| _ch == ch).unwrap();
-                    let (start, end) = (i+1, i+c+1);
-                    self.token_list.push_back(Token::VarString(&string[start..end]));
-                }
-                _ => {
-                    let remaining_str = &string[i..];
-                    if let Some(s) = starts_with_symbol(remaining_str) {
-                        self.token_list.push_back(Token::Symbols(s));
-                        for _ in 0..s.len(){
-                            it.next();
+                    '"' | '\'' => {
+                        // extract string literal in between quotes
+                        let c = it.position(|(_, _ch)| _ch == ch).unwrap();
+                        let (start, end) = (i + 1, i + c + 1);
+                        self.token_list
+                            .push_back(Token::VarString(&string[start..end]));
+                    }
+                    _ => {
+                        let remaining_str = &string[i..];
+                        if let Some(s) = starts_with_symbol(remaining_str) {
+                            self.token_list.push_back(Token::Symbols(s));
+                            for _ in 0..s.len() {
+                                it.next();
+                            }
+                        } else {
+                            omit_current_ch_for_capture = false;
+                            if !capturing {
+                                capturing = true;
+                                start = i;
+                            }
                         }
-                    } else {
-                        is_in_string = true;
                     }
-                },
-            } 
+                }
+
+                if capturing && omit_current_ch_for_capture {
+                    capturing = false;
+                    let end = i;
+                    if let Some(previous_token) = self.token_list.pop_back() {
+                        self.token_list
+                            .push_back(Token::VarString(&string[start..end]));
+                        self.token_list.push_back(previous_token);
+                    } else {
+                        self.token_list
+                            .push_back(Token::VarString(&string[start..end]));
+                    }
+                }
+                if capturing && it.peek().is_none() {
+                    capturing = false;
+                    self.token_list
+                        .push_back(Token::VarString(&string[start..]));
+                }
+            } else {
+                break;
+            }
         }
     }
 }
