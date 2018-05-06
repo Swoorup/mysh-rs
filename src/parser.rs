e94d8cbf719ba4dc49776d316c6459e24d8abb45
Swoorup Joshi <swoorupj@gmail.com>
2018-05-06 18:00:51 +0800

    unfinished parser implementation

--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,59 @@
-use LexicalAnalyzer;
+use std::collections::LinkedList;
 
-struct SyntaxTree {
-    left: Option<Box<SyntaxTree>>,
-    right: Option<Box<SyntaxTree>>,
+use lexer::Token;
+
+#[derive(Debug)]
+struct SyntaxTree<'a> {
+    left: Option<Box<SyntaxTree<'a>>>,
+    right: Option<Box<SyntaxTree<'a>>>,
+    token: &'a Token<'a>,
 }
 
-pub struct Parser {
-    ast: Option<SyntaxTree>,
-}
-
-impl Parser {
-    pub fn new() -> Parser {
-        Parser {
-            ast: None,
+impl<'a> SyntaxTree<'a> {
+    fn new(token: &'a Token) -> SyntaxTree<'a> {
+        SyntaxTree {
+            left: None,
+            right: None,
+            token: token,
         }
+    }
+
+    fn insert_left(&mut self, token: &'a Token) {
+        self.left = Some(Box::new(SyntaxTree {
+            left: None,
+            right: None,
+            token: token,
+        }));
+    }
+
+    fn insert_right(&mut self, token: &'a Token) {
+        self.right = Some(Box::new(SyntaxTree {
+            left: None,
+            right: None,
+            token: token,
+        }));
+    }
+}
+
+#[test]
+fn test_syntax_tree() {
+    let mut syntaxtree = SyntaxTree::new(&Token::WhiteSpace);
+    syntaxtree.insert_left(&Token::WhiteSpace);
+    syntaxtree.insert_right(&Token::WhiteSpace);
+    syntaxtree.right.as_mut().unwrap().insert_left(&Token::WhiteSpace);
+    println!("{:?}", syntaxtree);
+}
+
+pub struct Parser<'a> {
+    ast: Option<SyntaxTree<'a>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new() -> Parser<'a> {
+        Parser { ast: None }
+    }
+
+    pub fn parse(token_list: LinkedList<Token>) -> Result<(), String> {
+        Ok(())
     }
 }
