e94cf8338d802dc3e04076dc97ea384ac1a9ab9b
Swoorup Joshi <swoorupj@gmail.com>
2018-05-14 18:27:41 +0800

    shell signals

--- src/builtin.rs
+++ src/builtin.rs
@@ -1,8 +1,10 @@
+use nix::sys::signal::*;
 use std::sync::Mutex;
 use std::sync::MutexGuard;
 
 lazy_static! {
     static ref PROMPT: Mutex<String> = Mutex::new("λ ".to_string());
+    static ref OLD_SIGACTION: Mutex<Option<SigAction>> = Mutex::new(None);
 }
 
 pub fn set_prompt(prompt_str: &str) {
@@ -13,8 +15,25 @@ pub fn get_prompt() -> MutexGuard<'static, String> {
     PROMPT.lock().unwrap()
 }
 
+pub fn set_shell_signal_handlers() {
+    let actions = SigAction::new(SigHandler::SigIgn, SaFlags::empty(), SigSet::empty());
+    unsafe {
+        sigaction(Signal::SIGTSTP, &actions).unwrap();
+        sigaction(Signal::SIGQUIT, &actions).unwrap();
+        *OLD_SIGACTION.lock().unwrap() = Some(sigaction(Signal::SIGINT, &actions).unwrap())
+    }
+}
+
+pub fn disable_shell_signal_handlers() {
+    if let Some(actions) = &*OLD_SIGACTION.lock().unwrap() {
+        unsafe {
+            sigaction(Signal::SIGINT, &actions).unwrap();
+        }
+    }
+}
+
 #[test]
-fn test_prompt(){
+fn test_prompt() {
     set_prompt("λ ");
     assert!(&(*get_prompt()) == "λ ");
 
